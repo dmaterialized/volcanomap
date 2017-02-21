d9421146b435dd3a4ef8c64c492fccd2de155178
David-Michael Cook <dmaterialized@gmail.com>
2017-02-21 00:05:34 -0500

    add elevation marker color

--- script1.py
+++ script1.py
@@ -5,22 +5,15 @@ import pandas
 # initialize pandas
 df=pandas.read_csv("Volcanoes-USA.txt")
 
-    #initialize folium
-
-
+#initialize folium
 map=folium.Map(location=[45.362,-121.697],zoom_start=4,tiles='Stamen Terrain')
 
-# add markers
-#map.simple_marker(location=[45.3288,-121.6625],popup='Mt. Hood Meadows', marker_color='red')
-#map.simple_marker(location=[45.3311,-121.7311],popup='Timberlake Lodge', marker_color='green')
-
+#create all markers
+for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
+    map.simple_marker(location=[lat,lon],popup=name, marker_color='green' if elev in range(0,1000) else 'white' if elev in range(1000,2000) else 'orange' if elev in range(2000,3000) else 'red')
 
 # add Portland for fun
 map.simple_marker(location=[45.5231,-122.6765],popup='Portland, OR', marker_color='white')
-
-for lat,lon,name in zip(df['LAT'],df['LON'],df['NAME']):
-    map.simple_marker(location=[lat,lon],popup=name, marker_color='blue')
-
 
 # build the file
 map.create_map(path='pymap.html')
