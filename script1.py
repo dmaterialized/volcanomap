c75eee01104bfb69edca60721006add3fd3c47c6
David-Michael Cook <dmaterialized@gmail.com>
2017-02-25 00:18:48 -0500

    add stepping, view averaging, refactor elev funcs

--- script1.py
+++ script1.py
@@ -1,19 +1,18 @@
 #creates volcano map
 import folium
 import pandas
-
 # initialize pandas
 df=pandas.read_csv("Volcanoes-USA.txt")
-
 #initialize folium
-map=folium.Map(location=[45.362,-121.697],zoom_start=4,tiles='Stamen Terrain')
-
+map=folium.Map(location=[df['LAT'].mean(),df['LON'].mean()],zoom_start=6,tiles='Stamen Terrain')
 def color(elev):
-    if elev in range(0,1000):
+    minimum=int(min(df['ELEV']))
+    step=int((max(df['ELEV'])-min(df['ELEV']))/3)
+    if elev in range(minimum,minimum+step):
         col="green"
-    elif elev in range(1000,2000):
+    elif elev in range(minimum+step,minimum+step*2):
         col="white"
-    elif elev in range(2000,3000):
+    elif elev in range(minimum+step*2,minimum+step*3):
         col='orange'
     else:
         col='red'
@@ -21,7 +20,7 @@ def color(elev):
 
 #create all markers
 for lat,lon,name,elev in zip(df['LAT'],df['LON'],df['NAME'],df['ELEV']):
-    map.simple_marker(location=[lat,lon],popup=name, marker_color=color(elev))
+    map.simple_marker(location=[lat,lon],popup=name,marker_color=color(elev))
 
 # add Portland for fun
 map.simple_marker(location=[45.5231,-122.6765],popup='Portland, OR', marker_color='white')
